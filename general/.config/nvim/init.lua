fd2bae9710b580c1a8bdd6e92b6decc58d9c6ef1
Golden_Water <2439577268@qq.com>
2024-02-18 18:10:24 +0800

    feat(general::.config/nvim): use coc.nvim

--- general/.config/nvim/init.lua
+++ general/.config/nvim/init.lua
@@ -46,24 +46,39 @@ require("lazy").setup({
   "junegunn/fzf",
 
   "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
 
-  "neovim/nvim-lspconfig",
-  "simrat39/rust-tools.nvim",
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   version = "^4",
+  --   ft = { "rust" },
+  -- },
+  
+  {
+    "neoclide/coc.nvim",
+    branch = "master",
+    build = "npm ci"
+  },
 
-  "hrsh7th/nvim-cmp",
-
-  "hrsh7th/cmp-nvim-lsp",
-
-  "hrsh7th/cmp-nvim-lua",
-  "hrsh7th/cmp-nvim-lsp-signature-help",
-  "hrsh7th/cmp-vsnip",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/vim-vsnip",
-
-  "nvim-treesitter/nvim-treesitter",
+  -- "nvim-treesitter/nvim-treesitter",
 })
+-- endregion
+
+-- region coc.nvim
+vim.g.coc_global_extensions = {
+  "coc-sh",
+
+  "coc-rust-analyzer",
+  "coc-lua",
+  "coc-tsserver",
+  "coc-sql",
+  "@yaegassy/coc-pylsp",
+
+  "coc-toml",
+  "coc-json",
+
+  "coc-spell-checker",
+}
+require("coc_init")
 -- endregion
 
 -- region mason
@@ -76,119 +91,33 @@ require("mason").setup({
         },
     }
 })
-require("mason-lspconfig").setup()
 
-local rust_tools = require("rust-tools")
-rust_tools.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
--- endregion
-
--- region lsp diagnostics
-local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
-  })
-end
-
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
-sign({name = 'DiagnosticSignHint', text = ''})
-sign({name = 'DiagnosticSignInfo', text = ''})
-
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
-    float = {
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-    },
-})
-
-vim.cmd([[
-set signcolumn=yes
-autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-]])
--- endregion
-
--- cmp plugin
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    }),
-    ['<Tab>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    })
-  },
-  sources = {
-    { name = 'path' },
-    { name = 'nvim_lsp', keyword_length = 3 },
-    { name = 'nvim_lsp_signature_help'},
-    { name = 'nvim_lua', keyword_length = 2},
-    { name = 'buffer', keyword_length = 2 },
-    { name = 'vsnip', keyword_length = 2 },
-    { name = 'calc'},
-  },
-  window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-      fields = {'menu', 'abbr', 'kind'},
-      format = function(entry, item)
-          local menu_icon ={
-              nvim_lsp = 'λ',
-              vsnip = '⋗',
-              buffer = 'Ω',
-              path = '🖫',
-          }
-          item.menu = menu_icon[entry.source.name]
-          return item
-      end,
-  },
-})
+-- require("mason-lspconfig").setup()
+-- 
+-- local rust_tools = require("rust-tools")
+-- rust_tools.setup({
+--   server = {
+--     on_attach = function(_, bufnr)
+--       vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+--       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--     end,
+--   },
+-- })
 -- endregion
 
 -- region treesitter
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting=false,
-  },
-  ident = { enable = true }, 
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  }
-}
+-- require('nvim-treesitter.configs').setup {
+--   ensure_installed = { "lua", "rust", "toml" },
+--   auto_install = true,
+--   highlight = {
+--     enable = true,
+--     additional_vim_regex_highlighting=false,
+--   },
+--   ident = { enable = true }, 
+--   rainbow = {
+--     enable = true,
+--     extended_mode = true,
+--     max_file_lines = nil,
+--   }
+-- }
 -- endregion
