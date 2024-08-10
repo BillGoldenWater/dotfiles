ec01294972c7403dc446a70e5ff9bc4a48adb080
Golden_Water <golden_water@chaosw.site>
2024-08-10 16:24:31 +0800

    feat(general::.config::nvim::plugins::which-key): update which-key spec

--- general/.config/nvim/lua/golden_water/plugins/which-key.lua
+++ general/.config/nvim/lua/golden_water/plugins/which-key.lua
@@ -6,16 +6,16 @@ return {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]ab', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>b', group = '[B]uffer' },
+        { '<leader>t', group = '[T]ab' },
 
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>g', group = '[G]it' },
       }
     end,
   },
