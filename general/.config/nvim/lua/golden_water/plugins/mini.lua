ee86dc4c934a674ec93b036e02db318a7167f8d1
Golden_Water <golden_water@chaosw.site>
2025-04-25 17:24:12 +0800

    feat(general::.config::nvim): change to use nvim-surround for surround operation

--- general/.config/nvim/lua/golden_water/plugins/mini.lua
+++ general/.config/nvim/lua/golden_water/plugins/mini.lua
@@ -10,19 +10,6 @@ return {
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup {
-        n_lines = 500,
-        respect_selection_type = false,
-        mappings = {
-          update_n_lines = '',
-        },
-      }
-
       require('mini.align').setup {}
 
       -- Simple and easy statusline.
