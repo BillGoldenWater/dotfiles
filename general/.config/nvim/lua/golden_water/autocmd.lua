ed4fdc329f93e54394ecf7b91f443fc1341036a8
Golden_Water <golden_water@chaosw.site>
2025-12-04 13:52:06 +0800

    feat(general::.config::nvim::autocmd): binary editing using xxd

--- general/.config/nvim/lua/golden_water/autocmd.lua
+++ general/.config/nvim/lua/golden_water/autocmd.lua
@@ -28,3 +28,40 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
     vim.bo.filetype = 'glsl'
   end,
 })
+
+local group_binary = vim.api.nvim_create_augroup('goldenwater-binary', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
+  group = group_binary,
+  callback = function()
+    if vim.bo.binary then
+      vim.fn.execute '%!xxd -c 16'
+      vim.bo.filetype = 'xxd'
+      vim.fn.execute 'redraw'
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = group_binary,
+  callback = function()
+    if vim.bo.binary then
+      if vim.bo.filetype == 'xxd' then
+        vim.b.__gw_binary__ = vim.fn.winsaveview()
+        vim.fn.execute '%!xxd -r -c 16'
+      end
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = group_binary,
+  callback = function()
+    if vim.bo.binary then
+      if vim.bo.filetype == 'xxd' then
+        vim.fn.execute '%!xxd -c 16'
+        vim.bo.modified = false
+        vim.fn.winrestview(vim.b.__gw_binary__)
+        vim.b.__gw_binary__ = nil
+        vim.fn.execute 'redraw'
+      end
+    end
+  end,
+})
